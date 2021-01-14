172926f67a3f6c844cce093ab54a97f616e4367e
Rodica Fazakas <11833084+rodicafazakas@users.noreply.github.com>
2021-01-14 23:38:02 +0100

    Make contact page dynamic

--- web_server/server.py
+++ web_server/server.py
@@ -1,25 +1,59 @@
-from flask import Flask, render_template
+
+from flask import Flask, render_template, url_for, request, redirect
+import csv
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
+def my_home():
     return render_template('index.html')
 
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
 
 
-@app.route('/favicon.ico')
-def blog():
-    return 'These are my thoughts on blogs'
+# def write_to_file(data):
+#     with open('database.txt', mode='a') as database:
+#         email = data["email"]
+#         subject = data["subject"]
+#         message = data["message"]
+#         file = database.write(f'\n{email},{subject},{message}')
 
 
-@app.route('/blog/2020/dogs')
-def blog2():
-    return 'This is my dog'
+def write_to_csv(data):
+    with open('database.csv', newline="", mode='a') as database2:
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        csv_writer = csv.writer(database2, delimiter=",", quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email,subject,message])
+
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == "POST":
+        data = request.form.to_dict()
+        write_to_csv(data)
+        return redirect('/thankyou.html')
+    else:
+        return 'something went wrong'
+
+# @app.route('/about')
+# def about():
+#     return render_template('about.html')
+
+
+# @app.route('/favicon.ico')
+# def blog():
+#     return 'These are my thoughts on blogs'
+
+
+# @app.route('/components.html')
+# def blog2():
+#     return render_template('components.html')
+
 # set FLASK_APP=server.py
 # set FLASK_ENV=development
 # flask run
